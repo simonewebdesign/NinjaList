b462b0d5a9876cd3c6c25a860a6442d14faee73d
nicky1525 <nicky_1525@hotmail.it>
2016-01-01 15:36:44 +0000

    From array

--- NinjaList/ViewController.swift
+++ NinjaList/ViewController.swift
@@ -11,27 +11,27 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    var tasks:Array<String>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tasks = ["first task", "second task", "third task"]
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell")!
-        cell.textLabel?.text = "first task"
+        cell.textLabel?.text = tasks[indexPath.row]
         return cell
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return tasks.count
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
